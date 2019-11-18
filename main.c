bb25e5a1dfdbb47bc77a5f3865631d7db27f29ec
mushut <mushut@gmail.com>
2019-11-18 21:41:44 +0200

    Some more code

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+struct exercise {
+	char *name;
+	int series;
+	int reps;
+	int weight;
+};
+
+struct exercises {
+	struct exercise *allExercises;
+	char *date;
+	int durationInMinutes;
+};
+
+int adding(void) {
+	printf("Adding exercises\n");
+
+	return 0;
+}
+
 int main(void) {
 	printf("gym-c\n");
 
@@ -11,6 +30,9 @@ int main(void) {
 		if (input != NULL && result != EOF) {
 			if (strcmp(input, "quit") != 0) {
 				printf("%s\n", input);
+				if (strcmp(input, "add") == 0) {
+					adding();
+				}
 			}
 			else {
 				break;
